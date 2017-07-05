d20c938c5f2704dd89074fa9a87a5b2bbda00faf
fgarcia <fernando.garcia@tecnilogica.com>
2017-07-05 08:58:17 +0200

    Score test

--- WhatIsThisColorTests/ScoreTest.swift
+++ WhatIsThisColorTests/ScoreTest.swift
@@ -11,39 +11,15 @@ import XCTest
 
 class ScoreTest: XCTestCase {
     
-    var score: Score! = nil
+    // create a Score instace = nil
     
     override func setUp() {
         super.setUp()
-        score = Score()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // init Score instace
     }
-    
-    func testInitNotNilScore(){
-        XCTAssertNotNil(score, "The initialization score method works")
-    }
-    
-    func testIncreasePointNotEqualToPreviusPoint(){
-        let point = score.points
-        score.increasePoints()
-        XCTAssertNotEqual(point, score.points)
-    }
-    
-    func testIncreasePoint(){
-        let point = score.points
-        score.increasePoints()
-        XCTAssertEqual((point + 1), score.points)
-    }
-    
-    func testFailEqualZero(){
-        score.fail()
-        XCTAssertEqual(score.points, 0)
-    }
-    
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        score = nil
+        // Remove Score instace
         super.tearDown()
     }
     
